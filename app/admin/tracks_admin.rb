fd7ff6449222c18fc015f0433166a41c0853dd65
Francisco Barreto <francisco.barreto@wrightia.com>
2020-07-08 10:19:27 +0100

    tracks

--- app/admin/tracks_admin.rb
+++ app/admin/tracks_admin.rb
@@ -1,6 +1,11 @@
 Trestle.resource(:tracks) do
   # Customize the table columns shown on the index view.
   #
+
+  menu do
+    item :tracks, icon: "fa fa-music"
+  end
+
   table do
     column :title
     actions
@@ -8,14 +13,11 @@ Trestle.resource(:tracks) do
 
   # Customize the form fields shown on the new/edit views.
   #
-  # form do |track|
-  #   text_field :name
-  #
-  #   row do
-  #     col { datetime_field :updated_at }
-  #     col { datetime_field :created_at }
-  #   end
-  # end
+  form do |track|
+    text_field :title
+
+    select :movie_id, Movie.all, { label: "Movie" }
+  end
 
   # By default, all parameters passed to the update and create actions will be
   # permitted. If you do not have full trust in your users, you should explicitly
